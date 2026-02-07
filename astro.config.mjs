104a4594c9ff76fe60199075481390d30b4c4319
Zerrie Mack <mackzerrie@gmail.com>
2026-02-07 00:49:08 -0600

    Enable Astro server output on Vercel + prisma generate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://millymakertracker.com",
+  output: "server",
+  adapter: vercel(),
+  integrations: [mdx(), sitemap()],
 });
