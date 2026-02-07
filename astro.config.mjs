d6e5fd8e9e21989f2cd83065b456e2578062a62d
Zerrie Mack <mackzerrie@gmail.com>
2026-02-07 00:51:50 -0600

    Fix Astro Vercel adapter import (SSR enabled)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
